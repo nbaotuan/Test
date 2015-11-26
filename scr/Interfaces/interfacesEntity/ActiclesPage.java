888ee379e48e2eea0843f97aaaacc42a2af9f3ff
tuan.nguyen <nbaotuan@gmail.com>
2015-11-26 09:52:35 +0700

    second commit

--- scr/Interfaces/interfacesEntity/ActiclesPage.java
+++ scr/Interfaces/interfacesEntity/ActiclesPage.java
@@ -4,5 +4,6 @@ public class ActiclesPage {
 	public static final String NEW_BUTTON = "//button[@class='btn btn-small btn-success']";
 	public static final String EDIT_BUTTON = "//div[@id='toolbar-edit']";
 	public static final String PUBLISH_BUTTON = "//div[@id='toolbar-publish']";
+	//adasdassa
 }
 	
